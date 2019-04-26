bcb5187d01695093a92b36262e0dbf895958767a
RithickB <49742736+RithickB@users.noreply.github.com>
2019-04-26 13:33:53 +0530

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,36 @@
+package array_in_java;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class sample {
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+        /* Declare second integer, double, and String variables. */
+        int a;
+        double b;
+        String c;
+        /* Read and save an integer, double, and String to your variables.*/
+        c=scan.nextLine();
+        a=scan.nextInt();
+        b=scan.nextDouble();
+        
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(i+a);
+        /* Print the sum of the double variables on a new line. */
+	System.out.println(d+b);
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+        System.out.println(s+c);
+        scan.close();
+    }
+}

979f7de3eb098fb6e680b8fb8ee9ad349b3ead93
RithickB <49742736+RithickB@users.noreply.github.com>
2019-05-04 13:14:14 +0530

    Create Day9_Recursion.java

---
+++ Day9_Recursion.java
@@ -0,0 +1,38 @@
+//Question:Find Factorial of given Number
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Day9_Recursion {
+    static int factorial(int n) {
+        int i;
+        int fact=1;
+        for(i=1;i<=n;i++)
+        {
+            fact=fact*i;
+        }
+        return fact;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int result = factorial(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
